263a27957378d863e5f071578c45511743cce9b9
SunwooKim11 <seonu2001@naver.com>
2023-05-17 10:40:32 +0900

    Make setup script and Delete needless file

---
+++ instagram_clone/setup.sh
@@ -0,0 +1,5 @@
+#django instagram clone coding setup
+# https://anaconda.org/conda-forge/django
+echo "setup instag=ram clone coding project..."
+conda install -c conda-forge django
+conda install -c conda-forge djangorestframework
